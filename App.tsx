e01fd70e51d8b64fd639d8828fd934eda356ad98
Synergism612 <1273059247@qq.com>
2023-04-13 14:01:51 +0800

    flex:learn Dimensions

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import Index from './src/02_FlexBox/FlexDirection';
-
+// import Index from './src/01_StyleSheet';
+// import Index from './src/02_FlexBox/FlexDirection';
+// import Index from './src/02_FlexBox/JustifyContent';
+// import Index from './src/02_FlexBox/AlignItems';
+import Index from './src/03_Dimensions';
 export default class App extends Component {
   render() {
     return <Index />;
